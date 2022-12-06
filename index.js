1054b3dc6351752859a4cf1e8213477aa21f61c6
EmilyBernard <bernam2000@hotmail.com>
2022-12-06 18:36:03 -0500

    added lib files

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const jest = require('jest');
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
