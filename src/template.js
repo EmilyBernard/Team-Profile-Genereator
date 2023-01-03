7ce4ae958b65274fbb12cfca11aa1b0256168848
EmilyBernard <bernam2000@hotmail.com>
2023-01-03 17:11:52 -0500

    added html to create team profile page and manager card

--- src/template.js
+++ src/template.js
@@ -12,8 +12,46 @@ module.exports = team =>{
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron text-white mb-3">
+                <h1 class="text-center">My Team</h1>  
+            </div>
+        </div>
+    </div>  
+    <div class="container">
+        <div class="row">
+            <div class="main-section col-12 d-flex justify-content-center">
+                ${createProfile(team)}
+            </div>
+        </div>
+    </div> 
 </body>
 </html>
-    `
+    `;
+};
+
+//Create Team Profile
+const createProfile = team => {
+
+//Create Manger Card
+const createManager = manager => {
+    return`
+    <div class="card employee-card manager-card">
+        <div class="card-header text-center">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h4 class="card-title">Title: ${manager.getTitle()}</h4>
+            </div>
+            <div class="card-body bg-light">
+                <ul class="list-group text-dark">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office number: <a href="tel:${manager.getOfficeNumber()}">${manager.getOfficeNumber()}</a></li>
+                </ul>
+            </div>
+        </div>
+    `;
+};
+
+
 }
