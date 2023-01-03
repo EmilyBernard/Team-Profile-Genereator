ab0756b5b50989b6cccc55874b02d236d4db4750
EmilyBernard <bernam2000@hotmail.com>
2023-01-03 16:45:16 -0500

    added lines 11-14 in indexjs file to connect indexhtml, dist path and templte file, also started the templatejs file

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
+
+const render = require('./src/template.js');
+
