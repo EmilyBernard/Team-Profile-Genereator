248922f18b217bf44002b017c510e439535e7e33
EmilyBernard <bernam2000@hotmail.com>
2023-01-30 12:10:10 -0500

    coded employee test js file

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,23 @@
+const Employee = require('..lib/employee');
+
+test('creates employee object', () => {
+    const employee = new Employee(Wanda);
+});
+
+test('set id with constructor', () => {
+    const testValue = 100;
+    const e = new Employee('Foo', testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test('set email with constructor', () => {
+    const testValue = 'employee@email.com';
+    const e = new Employee('Foo', 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test('getTitle() return Employee', () => {
+    const testValue = 'Employee';
+    const e = new Employee('Wanda', 1, 'employee@email.com');
+    expect(e.getTitle()).toBe(testValue);
+});
